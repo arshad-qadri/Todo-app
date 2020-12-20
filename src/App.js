459f120a7d4cbc5cb72ef6996d2056ec72feb153
arshad-qadri <arshadqadri321@gmail.com>
2020-12-20 19:32:09 +0530

    Uploading the todo app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import TodoList from "./component/TodoList"
+
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList/>
+
+    </div>
+  );
+}
+
+export default App;
